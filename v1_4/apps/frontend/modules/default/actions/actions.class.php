6ac4195b2f75aa34295c4f312c9c535291d904e9
Artur Daniel Melo <adsmelo@ua.pt>
2012-10-13 18:52:23 +0100

    1.4 - Added frontend default module

---
+++ v1_4/apps/frontend/modules/default/actions/actions.class.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * default actions.
+ *
+ * @package    stimuLearning
+ * @subpackage default
+ * @author     Artur Melo <adsmelo@ua.pt>
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class defaultActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+    //$this->forward('default', 'error404');
+  }
+  
+  /**
+   * Executes error404 action
+   *
+   * @param sfRequest $request A request object
+   *
+   * @return nothing
+   */
+  public function executeError404(sfWebRequest $request)
+  {
+  }
+}
---
+++ v1_4/apps/frontend/modules/default/templates/error404Success.php
@@ -0,0 +1,4 @@
+<?php echo "Página não encontrada. O servidor retornou um erro 404." ?>
+<br />
+<br />
+<?php echo "Por favor verifique o URL inserido..." ?>
