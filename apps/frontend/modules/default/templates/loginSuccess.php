3a24bb46ffd433d81cc8bd6a8699d937bdc95c10
Artur Daniel Melo <adsmelo@ua.pt>
2012-12-11 01:44:34 +0000

    S1 - Created market penetration chart with mouse hover and zoom.

--- apps/frontend/modules/default/templates/loginSuccess.php
+++ apps/frontend/modules/default/templates/loginSuccess.php
@@ -16,13 +16,16 @@
 Solicitar registo de uma nova conta.
 
 <br /><br />
-<div id="placeholder" style="width: 500px; height: 300px;"></div>
+<div style="float:left">
+  <div id="market_share_chart" style="width: 500px; height: 300px;"></div>
+</div>
 
-<p>One of the goals of Flot is to support user interactions. Try
-pointing and clicking on the points.</p>
+<div id="miniature" style="float:left;margin-left:20px">
+  <div id="overview" style="width:166px;height:100px"></div>
+
+  <p id="overviewLegend" style="margin-left:10px"></p>
+</div>
 
 <p id="hoverdata">Mouse hovers at
 (<span id="x">0</span>, <span id="y">0</span>). <span id="clickdata"></span></p>
 
-<p>A tooltip is easy to build with a bit of jQuery code and the
-data returned from the plot.</p>
--- web/js/marketShareChart.js
+++ web/js/marketShareChart.js
@@ -1,29 +1,38 @@
 jQuery(document).ready(function() {
-	numTicks = 36;
-	startingLevel = 5;
-	saturationLevel = 70;
-	alpha = 100;
-	beta = -0.3;
+	var numTicks = 36;
+	var startingLevel = 5.00;
+	var saturationLevel = 70.00;
+	var alpha = 100;
+	var beta = -0.3;
 	
-	var market = [0, startingLevel];
-	for (var i = 1; i <= numTicks; i++) {
-		
-	    var share = Math.round((startingLevel + ((saturationLevel - startingLevel) / (1 + alpha * Math.exp(i * beta)))), 2);
-		market.push([i, share]);
-	}
+	// setup plot
+    function getData() {
+    	var market = [];
+    	market.push([0, startingLevel]);
+    	for (var i = 0; i < numTicks; i++) {
+    	    var share = (startingLevel + ((saturationLevel - startingLevel) / (1 + alpha * Math.exp(i * beta)))).toFixed(2);
+    		market.push([i+1, share]);
+    	}
+    	
+        return [{ data: market, label: 'market penetration (%)' }];
+    };
+	
+	var startData = getData();
+	
+	var options = {
+        legend: { show: false },
+        series: {
+            lines: { show: true },
+            points: { show: true }
+        },
+		grid: { hoverable: true, clickable: true },
+		yaxis: { min: 0, max: 100 },
+        //yaxis: { ticks: 10 },
+		xaxis: { min: -0.5, max: numTicks+1 },
+        selection: { mode: "xy" }
+    };
 
-    var plot = jQuery.plot(jQuery('#placeholder'),
-    	[{
-    		data: market, label: 'market penetration (%)' 
-    	}], {
-    		series: {
-    			lines: { show: true },
-    			points: { show: true },
-    		},
-    		grid: { hoverable: true, clickable: true },
-    		yaxis: { min: 0, max: 100 },
-    		xaxis: { min: 0, max: i+1 },
-    	});
+    var plot = jQuery.plot(jQuery('#market_share_chart'), startData, options);
     
     function showTooltip(x, y, contents) {
         jQuery('<div id="tooltip">' + contents + '</div>').css( {
@@ -39,32 +48,77 @@ jQuery(document).ready(function() {
     };
     
     var previousPoint = null;
-    jQuery('#placeholder').bind('plothover', function (event, pos, item) {
+    jQuery('#market_share_chart').bind('plothover', function (event, pos, item) {
     	jQuery('#x').text(pos.x.toFixed(2));
     	jQuery('#y').text(pos.y.toFixed(2));
-        jQuery('#tooltip').remove();
         
         if (item) {
-        	alert('item');
             if (previousPoint != item.dataIndex) {
                 previousPoint = item.dataIndex;
+                jQuery('#tooltip').remove();
                 
-                var x = item.datapoint[0].toFixed(2),
-                    y = item.datapoint[1].toFixed(2);
+                var x = item.datapoint[0],
+                    y = item.datapoint[1];
                 
                 showTooltip(item.pageX, item.pageY,
-                            item.series.label + " of " + x + " = " + y);
+                            "Month #: "+ x +"<br>Penetration: " + y +' %');
             }
         }
         else {
+        	jQuery('#tooltip').remove();
             previousPoint = null;            
         }
     });
     
-    jQuery('#placeholder').bind('plotclick', function (event, pos, item) {
+    jQuery('#market_share_chart').bind('plotclick', function (event, pos, item) {
         if (item) {
         	jQuery('#clickdata').text("You clicked point " + item.dataIndex + " in " + item.series.label + ".");
             plot.highlight(item.series, item.datapoint);
         }
     });
+    
+  // ZOOMING
+    // setup overview
+    var overview = $.plot($("#overview"), startData, {
+        legend: { show: true, container: $("#overviewLegend") },
+        series: {
+            lines: { show: true, lineWidth: 1 },
+            shadowSize: 0
+        },
+        xaxis: { ticks: 4 },
+        yaxis: { ticks: 3, min: 0, max: 100 },
+        grid: { color: "#999" },
+        selection: { mode: "xy" }
+    });
+    
+    // now connect the two 
+    $("#market_share_chart").bind("plotselected", function (event, ranges) {
+        // clamp the zooming to prevent eternal zoom
+        if (ranges.xaxis.to - ranges.xaxis.from < 0.00001)
+            ranges.xaxis.to = ranges.xaxis.from + 0.00001;
+        if (ranges.yaxis.to - ranges.yaxis.from < 0.00001)
+            ranges.yaxis.to = ranges.yaxis.from + 0.00001;
+        
+        // do the zooming
+        plot = $.plot($("#market_share_chart"), getData(),
+                      $.extend(true, {}, options, {
+                          xaxis: { min: ranges.xaxis.from, max: ranges.xaxis.to },
+                          yaxis: { min: ranges.yaxis.from, max: ranges.yaxis.to }
+                      }));
+        
+        // don't fire event on the overview to prevent eternal loop
+        overview.setSelection(ranges, true);
+    });
+    
+    $("#overview").bind("plotselected", function (event, ranges) {
+        plot.setSelection(ranges);
+    });
+    
+    function resetZoom() 
+    { 
+    	plot=$.plot($("#market_share_chart"),getData(),options); 
+    	overview.clearSelection(); 
+    	plot.clearSelection(); 
+    };
+    $("#overview").bind("dblclick",resetZoom); 
 });
