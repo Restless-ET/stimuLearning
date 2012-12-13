3d77c44529980bca1ce6368d5475937bba8b7874
Artur Daniel Melo <adsmelo@ua.pt>
2012-12-13 22:29:13 +0000

    S1 - Adapted Scenario pages presentation in order to eliminate the need to re-defined the success templates for new and edit actions.

--- apps/frontend/modules/scenario/templates/_market_share_chart.php
+++ apps/frontend/modules/scenario/templates/_market_share_chart.php
@@ -1,19 +1,17 @@
-<div id="charts_container">
-  <div id="market_share_chart"></div>
+<div id="market_share_chart"></div>
 
-  <div id="miniature">
-    <div id="overview"></div>
-    <p id="overviewLegend" style="margin-left:10px"></p>
-    <p id="hoverdata">Mouse at
-    (<span id="x">0</span>, <span id="y">0</span>).</p>
-  </div>
+<div id="miniature">
+  <p id="overviewLegend"></p>
+  <div id="overview"></div>
 
-  <?php if (isset($scenario)): ?>
-    <input id="scenario_tick_alias" style="display: none;" type="text" value="<?php echo $scenario->getTickAlias(); ?>">
-    <input id="scenario_lifespan" style="display: none;" type="text" value="<?php echo $scenario->getLifespan(); ?>">
-    <input id="scenario_starting_level" style="display: none;" type="text" value="<?php echo $scenario->getStartingLevel(); ?>">
-    <input id="scenario_saturation_level" style="display: none;" type="text" value="<?php echo $scenario->getSaturationLevel(); ?>">
-    <input id="scenario_alpha" style="display: none;" type="text" value="<?php echo $scenario->getAlpha(); ?>">
-    <input id="scenario_beta" style="display: none;" type="text" value="<?php echo $scenario->getBeta(); ?>">
-  <?php endif; ?>
+  <p id="hoverdata" style="margin-left:5px"><br><br>Mouse at (<span id="x">0</span>, <span id="y">0</span>)</p>
 </div>
+
+<?php if (isset($scenario)): ?>
+  <input id="scenario_tick_alias" style="display: none;" type="text" value="<?php echo $scenario->getTickAlias(); ?>">
+  <input id="scenario_lifespan" style="display: none;" type="text" value="<?php echo $scenario->getLifespan(); ?>">
+  <input id="scenario_starting_level" style="display: none;" type="text" value="<?php echo $scenario->getStartingLevel(); ?>">
+  <input id="scenario_saturation_level" style="display: none;" type="text" value="<?php echo $scenario->getSaturationLevel(); ?>">
+  <input id="scenario_alpha" style="display: none;" type="text" value="<?php echo $scenario->getAlpha(); ?>">
+  <input id="scenario_beta" style="display: none;" type="text" value="<?php echo $scenario->getBeta(); ?>">
+<?php endif; ?>
