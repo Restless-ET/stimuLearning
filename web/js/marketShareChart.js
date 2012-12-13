27612f899e526e590a48dc26ed885dc38038caed
Artur Daniel Melo <adsmelo@ua.pt>
2012-12-13 01:51:04 +0000

    Updated market share chart to be dynamically updated.

--- web/js/marketShareChart.js
+++ web/js/marketShareChart.js
@@ -1,57 +1,36 @@
-jQuery(document).ready(function() {
-	var numTicks = parseInt(jQuery('#scenario_lifespan').val());
-	var startingLevel = parseFloat(jQuery('#scenario_starting_level').val());
-	var saturationLevel = parseFloat(jQuery('#scenario_saturation_level').val());
-	var alpha = parseInt(jQuery('#scenario_alpha').val());
-	var beta = parseFloat(jQuery('#scenario_beta').val());
-	
-	// setup plot
-    function getData() {
-    	var market = [];
-    	market.push([0, startingLevel]);
-    	for (var i = 0; i < numTicks; i++) {
-    	    var share = (startingLevel + ((saturationLevel - startingLevel) / (1 + alpha * Math.exp(i * beta)))).toFixed(2);
-    		market.push([i+1, share]);
-    	}
-    	
-        return [{ data: market, label: 'market penetration (%)' }];
-    };
-	
-	var startData = getData();
-	
-	var options = {
-        legend: { show: false },
-        series: {
-            lines: { show: true },
-            points: { show: true }
-        },
-		grid: {
-			hoverable: true,
-			//clickable: true
-		},
-		yaxis: { min: 0, max: 100 },
-        //yaxis: { ticks: 10 },
-		xaxis: { min: -0.5, max: numTicks + 1 },
-        selection: { mode: "xy" }
-    };
+chartDivId = '#market_share_chart';
+lifespanId = '#scenario_lifespan';
+startLevelId = '#scenario_starting_level';
+saturationId = '#scenario_saturation_level';
+alphaId = '#scenario_alpha';
+betaId = '#scenario_beta';
 
-    var plot = jQuery.plot(jQuery('#market_share_chart'), startData, options);
+var numTicks, startingLevel, saturationLevel, alpha, beta;
+var startData, options;
+
+jQuery(document).ready(function() {
+	
+	actOnBlur = jQuery([]).add($(lifespanId)).add($(startLevelId)).add($(saturationId)).add($(alphaId)).add($(betaId));
+	if (jQuery('#scenario_tick_alias').length != 0)
+	{
+		// hook on change events to each related input
+		actOnBlur.bind('blur', configAndInitiateGraphDraw);
+	}
+	
+	configAndInitiateGraphDraw();
+});
+
+function configAndInitiateGraphDraw() {
     
-    function showTooltip(x, y, contents) {
-        jQuery('<div id="tooltip">' + contents + '</div>').css( {
-            position: 'absolute',
-            display: 'block',
-            top: y + 5,
-            left: x + 5,
-            border: '1px solid #fdd',
-            padding: '2px',
-            'background-color': '#fee',
-            opacity: 0.80
-        }).appendTo('body').fadeIn(200);
-    };
+	setNeededValuesFromInput();
+	startData = getPlotData();
+	options = getGraphOptions();
+	
+	var previousPoint = null;
+
+    var plot = jQuery.plot(jQuery(chartDivId), startData, options);
     
-    var previousPoint = null;
-    jQuery('#market_share_chart').bind('plothover', function (event, pos, item) {
+    jQuery(chartDivId).bind('plothover', function (event, pos, item) {
     	jQuery('#x').text(pos.x.toFixed(2));
     	jQuery('#y').text(pos.y.toFixed(2));
         
@@ -73,29 +52,19 @@ jQuery(document).ready(function() {
         }
     });
     /*
-    jQuery('#market_share_chart').bind('plotclick', function (event, pos, item) {
+    jQuery(chartDivId).bind('plotclick', function (event, pos, item) {
         if (item) {
         	jQuery('#clickdata').text("You clicked point " + item.dataIndex + " in " + item.series.label + ".");
             plot.highlight(item.series, item.datapoint);
         }
     });
     */
-  // ZOOMING
-    // setup overview
-    var overview = $.plot($("#overview"), startData, {
-        legend: { show: true, container: $("#overviewLegend") },
-        series: {
-            lines: { show: true, lineWidth: 1 },
-            shadowSize: 0
-        },
-        xaxis: { ticks: 4 },
-        yaxis: { ticks: 3, min: 0, max: 100 },
-        grid: { color: "#999" },
-        selection: { mode: "xy" }
-    });
+    
+// ZOOMING
+    var overview = setupOverview();
     
     // now connect the two 
-    $("#market_share_chart").bind("plotselected", function (event, ranges) {
+    $(chartDivId).bind("plotselected", function (event, ranges) {
         // clamp the zooming to prevent eternal zoom
         if (ranges.xaxis.to - ranges.xaxis.from < 0.00001)
             ranges.xaxis.to = ranges.xaxis.from + 0.00001;
@@ -103,7 +72,7 @@ jQuery(document).ready(function() {
             ranges.yaxis.to = ranges.yaxis.from + 0.00001;
         
         // do the zooming
-        plot = $.plot($("#market_share_chart"), getData(),
+        plot = $.plot($(chartDivId), getPlotData(),
                       $.extend(true, {}, options, {
                           xaxis: { min: ranges.xaxis.from, max: ranges.xaxis.to },
                           yaxis: { min: ranges.yaxis.from, max: ranges.yaxis.to }
@@ -117,11 +86,79 @@ jQuery(document).ready(function() {
         plot.setSelection(ranges);
     });
     
-    function resetZoom() 
-    { 
-    	plot=$.plot($("#market_share_chart"),getData(),options); 
-    	overview.clearSelection(); 
-    	plot.clearSelection(); 
-    };
-    $("#overview").bind("dblclick",resetZoom); 
-});
+    $("#overview").bind("dblclick", resetZoom);
+}
+
+function setNeededValuesFromInput() {
+	numTicks = parseInt(jQuery(lifespanId).val());
+	startingLevel = parseFloat(jQuery(startLevelId).val());
+	saturationLevel = parseFloat(jQuery(saturationId).val());
+	alpha = parseInt(jQuery(alphaId).val());
+	beta = parseFloat(jQuery(betaId).val());
+}
+
+function getGraphOptions() {
+	var graphOptions = {
+	    legend: { show: false },
+	    series: {
+	        lines: { show: true },
+	        points: { show: true }
+	    },
+		grid: {
+			hoverable: true,
+			//clickable: true
+		},
+		yaxis: { min: 0, max: 100 },
+	    //yaxis: { ticks: 10 },
+		xaxis: { min: -0.5, max: numTicks + 1 },
+	    selection: { mode: "xy" }
+	};
+	
+	return graphOptions;
+}
+
+//setup plot data
+function getPlotData() {
+	var market = [];
+	market.push([0, startingLevel]);
+	for (var i = 0; i < numTicks; i++) {
+	    var share = (startingLevel + ((saturationLevel - startingLevel) / (1 + alpha * Math.exp(i * beta)))).toFixed(2);
+		market.push([i+1, share]);
+	}
+	
+    return [{ data: market, label: 'market penetration (%)' }];
+};
+
+function showTooltip(x, y, contents) {
+    jQuery('<div id="tooltip">' + contents + '</div>').css( {
+        position: 'absolute',
+        display: 'block',
+        top: y + 5,
+        left: x + 5,
+        border: '1px solid #fdd',
+        padding: '2px',
+        'background-color': '#fee',
+        opacity: 0.80
+    }).appendTo('body').fadeIn(200);
+};
+
+function setupOverview() {
+	return $.plot($("#overview"), startData, {
+        legend: { show: true, container: $("#overviewLegend") },
+        series: {
+            lines: { show: true, lineWidth: 1 },
+            shadowSize: 0
+        },
+        xaxis: { ticks: 4 },
+        yaxis: { ticks: 3, min: 0, max: 100 },
+        grid: { color: "#999" },
+        selection: { mode: "xy" }
+    });
+};
+
+function resetZoom() 
+{ 
+	plot = $.plot($(chartDivId), getPlotData(), options); 
+	overview.clearSelection(); 
+	plot.clearSelection(); 
+};
