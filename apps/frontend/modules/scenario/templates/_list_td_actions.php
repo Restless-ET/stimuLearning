d8616840e00868f2e1e03c629c73467474ce3770
Artur Daniel Melo <adsmelo@ua.pt>
2012-12-18 00:05:02 +0000

    Changed message on delete link for a scenario.

--- apps/frontend/modules/scenario/templates/_list_td_actions.php
+++ apps/frontend/modules/scenario/templates/_list_td_actions.php
@@ -4,10 +4,10 @@
       <?php echo link_to(__('Show', array(), 'messages'), 'scenario/show?id='.$scenario->getId(), array()) ?>
     </li>
   <?php if ($sf_user->hasCredential(array('admin')) && $scenario->status == 'Unstarted'): ?>
-    <?php echo $helper->linkToEdit($scenario, array(  'credentials' =>   array(    0 => 'admin',  ),  'params' =>   array(  ),  'class_suffix' => 'edit',  'label' => 'Edit',)) ?>
+    <?php echo $helper->linkToEdit($scenario, array('credentials' => array('admin'), 'params' => array(  ), 'class_suffix' => 'edit',  'label' => 'Edit',)) ?>
   <?php endif; ?>
   <?php if ($sf_user->hasCredential(array('admin')) && $scenario->status == 'Unstarted'): ?>
-    <?php echo $helper->linkToDelete($scenario, array(  'credentials' =>   array(    0 => 'admin',  ),  'params' =>   array(  ),  'confirm' => 'Are you sure?',  'class_suffix' => 'delete',  'label' => 'Delete',)) ?>
+    <?php echo $helper->linkToDelete($scenario, array('credentials' => array('admin'), 'confirm' => 'This removal is irreversible! Are you sure you want to proceed?', 'params' => array(), 'class_suffix' => 'delete',  'label' => 'Delete',)) ?>
   <?php endif; ?>
   </ul>
 </td>
