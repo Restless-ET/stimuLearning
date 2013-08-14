4415a8a6b4a9d7e7ac29e243cc465b3b140ada05
Artur Daniel Melo <adsmelo@ua.pt>
2013-08-14 17:54:02 +0100

    CS fixes

--- apps/frontend/modules/scenario/lib/scenarioGeneratorHelper.class.php
+++ apps/frontend/modules/scenario/lib/scenarioGeneratorHelper.class.php
@@ -9,12 +9,25 @@
  */
 class scenarioGeneratorHelper extends BaseScenarioGeneratorHelper
 {
+  /**
+   * Renders a custom link for an action to advance to the next step
+   *
+   * @param mixed $object The Scenario object
+   * @param array $params Array with parameters for the link to render
+   */
   public function linkToNextStep($object, $params)
   {
     return '<li class="sf_admin_action_sim_next_step">'.link_to(__($params['label'], array(), 'messages'),
             'scenario/advanceSimulation?id='.$object->getId().'&ticks=1',
             array('id' => 'scenario_sim_next_step')).'</li>';
   }
+
+  /**
+   * Renders a custom link for an action to advance to the next decision point
+   *
+   * @param mixed $object The Scenario object
+   * @param array $params Array with parameters for the link to render
+   */
   public function linkToNextDecision($object, $params)
   {
     //TODO calculate proper number of ticks
@@ -24,6 +37,13 @@ class scenarioGeneratorHelper extends BaseScenarioGeneratorHelper
             'scenario/advanceSimulation?id='.$object->getId().'&ticks='.$ticks,
             array('id' => 'scenario_sim_next_decision')).'</li>';
   }
+
+  /**
+   * Renders a custom link to run the simulation all at once
+   *
+   * @param mixed $object The Scenario object
+   * @param array $params Array with parameters for the link to render
+   */
   public function linkToFinish($object, $params)
   {
     //TODO calculate proper number of ticks
