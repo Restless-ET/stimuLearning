b4a203b95e9f566569ff8bf6bbf8ad8d7c667f3c
Artur Daniel Melo <adsmelo@ua.pt>
2013-09-07 17:43:03 +0100

    Some CSS improvements.

--- apps/frontend/templates/_title.php
+++ apps/frontend/templates/_title.php
@@ -1,18 +1,6 @@
-<div id="title">
-<h1>
 <?php $currentModule = sfContext::getInstance()->getModuleName(); ?>
 <?php if ($currentModule == 'operator'): ?>
-  <?php echo link_to('Operators', '@operator') ?>
+  <?php echo '<div id="title"><h1>'.link_to('Operators', '@operator').'</h1></div>'; ?>
 <?php elseif ($currentModule == 'scenario'): ?>
-  <?php echo link_to('Scenarios', '@scenario') ?>
-<?php elseif ($currentModule == 'technology'): ?>
-  <?php echo link_to('Technologies', '@technology') ?>
-<?php elseif ($currentModule == 'architecture'): ?>
-  <?php echo link_to('Architectures', '@architecture') ?>
-<?php elseif ($currentModule == 'equipment'): ?>
-  <?php echo link_to('Equipments', '@equipment') ?>
-<?php elseif($currentModule=='user'): ?>
-  <?php echo link_to('Users', '@user') ?>
+  <?php echo '<div id="title"><h1>'.link_to('Scenarios', '@scenario').'</h1></div>'; ?>
 <?php endif; ?>
-</h1>
-</div>
