8c64dabca603b3ea725a4fb305417e52dffcc977
Artur Daniel Melo <adsmelo@ua.pt>
2013-09-09 23:54:08 +0100

    Fixed JS calculation of number of ticks between decision points.

--- web/js/calculos.js
+++ web/js/calculos.js
@@ -1,12 +1,13 @@
 function setTicksBetweenDP() {
-	var ticks = 0;
-	var total_ticks = parseInt(jQuery('#scenario_lifespan').val());
-	var nbr_dp = parseInt(jQuery('#scenario_total_decision_points').val());
-	
-	if (nbr_dp> 0 && total_ticks >= nbr_dp)
-		ticks = Math.floor(total_ticks / nbr_dp);
-	
-	jQuery('#scenario_ticks_between_decision_points').val(ticks);
+    var ticks = 0;
+    var total_ticks = parseInt(jQuery('#scenario_lifespan').val());
+    var nbr_dp = parseInt(jQuery('#scenario_total_decision_points').val());
+
+    if (nbr_dp> 0 && total_ticks >= nbr_dp) {
+        ticks = Math.floor(total_ticks / (nbr_dp + 1));
+    }
+
+    jQuery('#scenario_ticks_between_decision_points').val(ticks);
 }
 
 function formatMoney(val, decPlaces, thouSeparator, decSeparator) {
