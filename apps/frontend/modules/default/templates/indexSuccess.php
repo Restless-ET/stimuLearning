a698d8475e5c0d31e798ded0f656917305086e05
Artur Daniel Melo <adsmelo@ua.pt>
2013-09-24 20:42:15 +0100

    Update welcome page text.

--- apps/frontend/modules/default/templates/indexSuccess.php
+++ apps/frontend/modules/default/templates/indexSuccess.php
@@ -10,12 +10,19 @@
 
 <a href="<?php echo url_for('@login'); ?>">Login</a>
 <br /><br />
-<p>This application is a user-friendly educational tool intended to reflect the market dynamics on the telecom industry.</p>
-<p>This tool is aimed to help telecom engineering students to learn how the technical decisions affect the ecnonomical outcomes, on telecom business models.</p>
-<p>The development of this tool is based on years of real-world analysis on telecom business models and their influences on the engineering decisions of a telecom operator.</p><br />
-<p style="width:45%; float: left;">After years of mathematical model development, we reached the model ilustrated on the right. <br /><br />
- We may say that the market is the main block of this tool.<br /><br />
- This application takes, as input, the decisions of the telecom operators, the actors. <br /><br />
- After processing these inputs, the system outputs the economic outcomes of each player.<br /><br />
- These outcomes can be affected by some parameters which model the behaviour of the market.</p>
+<p>This application is a user-friendly educational tool intended to reflect the market dynamics on the telecom industry.<br />
+   This tool is aimed to help telecom engineering students to learn how the technical decisions affect the ecnonomical outcomes, on telecom business models.<br />
+   The development of this tool is based on years of real-world analysis on telecom business models and their influences on the engineering decisions of a telecom operator.</p>
+<br />
+<p>We may say that the market is the main block of this tool.<br />
+   This application takes, as input, the decisions of the telecom operators, the actors. <br />
+   After processing these inputs, the system outputs the economic outcomes of each player.<br />
+   These outcomes can be affected by some parameters which model the behaviour of the market.</p>
+<br />
+<p>The player becomes a telecom operator, which is challenged to reach customers in
+    a new region, facing some competition during the process.<br />
+   The operator is able to setup subscription packages, to choose the network architecture that
+    better suits his offer and to change his options along the simulation time.<br />
+   The main goal is to achieve the greater market share in the end of the
+    simulation.</p>
 <?php endif; ?>
