b5f4d80d45c2d191d86eedf82bae68f73c4cd3c4
Artur Daniel Melo <adsmelo@ua.pt>
2013-11-06 20:30:03 +0000

    Close #16 Removed schema for deprecated class ScenarioTechnology

--- apps/frontend/modules/scenario/templates/_list_td_actions.php
+++ apps/frontend/modules/scenario/templates/_list_td_actions.php
@@ -2,10 +2,10 @@
   <ul class="sf_admin_td_actions fg-buttonset fg-buttonset-single">
     <?php echo $helper->linkToShow($scenario, array(  'params' => 'class=fg-button-mini fg-button ui-state-default fg-button-icon-left ',  'class_suffix' => 'show',  'label' => 'Show',  'ui-icon' => '',)) ?>
 
-    <?php if (!$scenario->started && $sf_user->hasCredential(array('manager'))): ?>
+    <?php if (!$scenario->started && ($sf_user->getAttribute('id') == $scenario->responsible_id || $sf_user->hasCredential(array('manager')))): ?>
       <?php echo $helper->linkToEdit($scenario, array('credentials' => array('manager'), 'params' => 'class=fg-button-mini fg-button ui-state-default fg-button-icon-left ', 'class_suffix' => 'edit', 'label' => 'Edit', 'ui-icon' => '',)) ?>
     <?php endif; ?>
-    <?php if (!$scenario->started && $sf_user->hasCredential(array('manager'))): ?>
+    <?php if (!$scenario->started && ($sf_user->getAttribute('id') == $scenario->responsible_id || $sf_user->hasCredential(array('manager')))): ?>
       <?php echo $helper->linkToDelete($scenario, array('credentials' => array('manager'), 'confirm' => 'This removal is irreversible! Are you sure you want to proceed?', 'params' => 'class=fg-button-mini fg-button ui-state-default fg-button-icon-left ',  'class_suffix' => 'delete',  'label' => 'Delete',  'ui-icon' => '',)) ?>
     <?php endif; ?>
   </ul>
