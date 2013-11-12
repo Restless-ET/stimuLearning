68f23314b7bcaef5dd82cda6e70d2cb737d81854
Artur Daniel Melo <adsmelo@ua.pt>
2013-11-12 12:04:21 +0000

    Update sfAdminThemejRollerPlugin skeletons for _form_field and _form_fieldset templates.

--- plugins/sfAdminThemejRollerPlugin/data/generator/sfDoctrineModule/jroller/template/templates/_form_fieldset.php
+++ plugins/sfAdminThemejRollerPlugin/data/generator/sfDoctrineModule/jroller/template/templates/_form_fieldset.php
@@ -2,6 +2,7 @@
   [?php foreach ($fields as $name => $field): ?]
     [?php if ((isset($form[$name]) && $form[$name]->isHidden()) || (!isset($form[$name]) && $field->isReal())) continue ?]
     [?php include_partial('<?php echo $this->getModuleName() ?>/form_field', array(
+      '<?php echo $this->getModuleName() ?>' => $<?php echo $this->getModuleName() ?>,
       'name'       => $name,
       'attributes' => $field->getConfig('attributes', array()),
       'label'      => $field->getConfig('label'),
