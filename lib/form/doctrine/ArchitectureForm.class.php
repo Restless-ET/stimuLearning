5ee875907f545d493d89f39337b6119699a7a7f5
Artur Daniel Melo <adsmelo@ua.pt>
2013-11-12 16:50:24 +0000

    Closes #25 Enable custom image uploading for Architectures.

--- lib/form/doctrine/ArchitectureForm.class.php
+++ lib/form/doctrine/ArchitectureForm.class.php
@@ -21,5 +21,29 @@ class ArchitectureForm extends BaseArchitectureForm
     public function configure()
     {
         unset($this['created_at'], $this['updated_at']);
+
+        //show upload selection and file removal option
+        $anchorStart = '<a href="%file%" target="_blank">';
+        $inputWidgetTemplate = '</a><br />%input%';
+        $delete = '<br />%delete% remove the current image';
+
+        $currentData = $this->getObject();
+        $this->setWidget('image', new sfWidgetFormInputFileEditable(array(
+          'file_src' => '/uploads/architecture/'.$this->getObject()->image,
+          'is_image' => true,
+          'edit_mode' => $currentData->image ? true : false,
+          'with_delete' => true,
+          'template' => $currentData->image ? '<div>%file%<br />%input%'.$delete.'</div>' :
+             $anchorStart.$currentData->image.$inputWidgetTemplate,
+        )));
+
+        $this->setValidator('image', new sfValidatorFile(array(
+          'required' => false,
+          'path' => sfConfig::get('sf_upload_dir').'/architecture/',
+          'max_size' => 1024, // Allowed size in bytes
+          'mime_types' => 'web_images',
+        )));
+
+        $this->setValidator('image_delete', new sfValidatorPass());
     }
 }
--- lib/model/doctrine/Architecture.class.php
+++ lib/model/doctrine/Architecture.class.php
@@ -11,4 +11,44 @@
  */
 class Architecture extends BaseArchitecture
 {
+  /**
+   * Generates a proper name for the uploaded file through the widget "image".
+   * If it's detected a file with the same name on the general uploads directory,
+   * it's appended one digit at the beginning of the file name
+   *
+   * @param string $file Original name (with extension) of the uploaded file
+   *
+   * @return string File name
+   */
+  public function generateImageFilename($file)
+  {
+    return $this->generateFilename($file);
+  }
+
+  /**
+   * Auxiliary function to be used by all the previous declared "generateFilename" functions
+   *
+   * @param string $file File original name (with extension) of the uploaded file
+   *
+   * @return string File name (according the established nomenclature standard)
+   */
+  private function generateFilename($file)
+  {
+    // get the original file name and the file extension
+    $originalName = $file->getOriginalName();
+
+    $newFileName = $originalName;
+
+    $filePath = sfConfig::get('sf_upload_dir').DIRECTORY_SEPARATOR.'architecture';
+
+    $i = 0;
+    while (file_exists($filePath.DIRECTORY_SEPARATOR.$newFileName)) {
+      $i++;
+      // create a new file name - append "i_" to the start of the original file name
+      $newFileName = $i.'_'.$originalName;
+    }
+
+    return $newFileName;
+  }
+
 }
