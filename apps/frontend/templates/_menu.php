5c355559eef542d27121470a1d8e3b36e3842535
Artur Daniel Melo <adsmelo@ua.pt>
2013-11-13 11:01:13 +0000

    Closes #29 Admin menu and sub-menu are now positioned on the right.

--- apps/frontend/templates/_menu.php
+++ apps/frontend/templates/_menu.php
@@ -5,7 +5,10 @@
   <div class="admin-menu">
   <?php if($sf_user->isAuthenticated()): ?>
     <ul>
-    <?php if($sf_user->hasCredential('admin')): ?>
+    <?php if ($sf_user->hasCredential('admin')): ?>
+      <li<?php if(in_array($currentModule, array('libTech','libArch','libEquip'))): ?> class="active"<?php endif; ?>>
+        <?php echo link_to('Library', '@user') ?>
+      </li>
       <li<?php if($currentModule == 'user'): ?> class="active"<?php endif; ?>>
         <?php echo link_to('Users', '@user') ?>
       </li>
@@ -25,7 +28,7 @@
       <li<?php if ($currentModule == 'default' && $currentAction == 'about'): ?> class="active"<?php endif; ?>>
         <?php echo link_to('About', '@about') ?>
       </li>
-    <?php if($sf_user->isAuthenticated()): ?>
+    <?php if ($sf_user->isAuthenticated()): ?>
       <?php if ($scenarioId): ?>
         <li<?php if($currentModule == 'scenario'): ?> class="active"<?php endif; ?>>
           <?php echo link_to('Scenario', 'scenario/show?id='.$scenarioId); ?>
@@ -45,28 +48,47 @@
     </ul>
   </div>
   <div id="sub-menu">
-  <?php if ($scenarioId && in_array($currentModule, array('technology','architecture','equipment'))): ?>
-    <div class="sub-main">
+  <?php if ($sf_user->hasCredential('admin')): ?>
+    <?php if (in_array($currentModule, array('libTech','libArch','libEquip'))): ?>
+    <div class="sub-admin">
       <ul>
-        <li<?php if($currentModule == 'technology'): ?> class="active"<?php endif; ?>>
+        <li<?php if ($currentModule == 'libTech'): ?> class="active"<?php endif; ?>>
           <?php echo link_to('Technologies', '@technology') ?>
         </li>
-        <li<?php if($currentModule == 'architecture'): ?> class="active"<?php endif; ?>>
+        <li<?php if ($currentModule == 'libArch'): ?> class="active"<?php endif; ?>>
           <?php echo link_to('Architectures', '@architecture') ?>
         </li>
-        <li<?php if($currentModule == 'equipment'): ?> class="active"<?php endif; ?>>
+        <li<?php if ($currentModule == 'libEquip'): ?> class="active"<?php endif; ?>>
           <?php echo link_to('Equipments', '@equipment') ?>
         </li>
       </ul>
     </div>
-  <?php elseif ($scenarioId && in_array($currentModule, array('operator','service'))): ?>
+    <?php endif; ?>
+  <?php endif; ?>
+  <?php if ($scenarioId): ?>
+    <?php if (in_array($currentModule, array('technology','architecture','equipment'))): ?>
     <div class="sub-main">
       <ul>
-        <li<?php if($currentModule == 'service'): ?> class="active"<?php endif; ?>>
+        <li<?php if ($currentModule == 'technology'): ?> class="active"<?php endif; ?>>
+          <?php echo link_to('Technologies', '@technology') ?>
+        </li>
+        <li<?php if ($currentModule == 'architecture'): ?> class="active"<?php endif; ?>>
+          <?php echo link_to('Architectures', '@architecture') ?>
+        </li>
+        <li<?php if ($currentModule == 'equipment'): ?> class="active"<?php endif; ?>>
+          <?php echo link_to('Equipments', '@equipment') ?>
+        </li>
+      </ul>
+    </div>
+    <?php elseif (in_array($currentModule, array('operator','service'))): ?>
+    <div class="sub-main">
+      <ul>
+        <li<?php if ($currentModule == 'service'): ?> class="active"<?php endif; ?>>
           <?php echo link_to('Services', '@service') ?>
         </li>
       </ul>
     </div>
-  <?php endif; ?>
+    <?php endif; ?>
+  <?php endif;?>
   </div>
 </div>
