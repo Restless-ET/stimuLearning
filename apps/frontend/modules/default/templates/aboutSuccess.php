f4e0c5d31b9a13aefe1edc296f590edf56086c9e
Artur Daniel Melo <adsmelo@ua.pt>
2013-11-13 10:42:45 +0000

    See #23 Add some text to 'About' page.

--- apps/frontend/modules/default/templates/aboutSuccess.php
+++ apps/frontend/modules/default/templates/aboutSuccess.php
@@ -1,4 +1,25 @@
 <div id="about">
-  <p>This page is still under construction.</p>
-  <br />
+  <p style="color: red;">This page is still under construction!!!</p>
+  <h2>The Problem</h2>
+  <p>The player becomes a telecom operator, which is challenged to reach customers in
+      a new region, facing some competition during the process. </p>
+  <p>The operator is able to setup tariffs, to choose the network architecture that
+      better suits his offer and to change his options along the simulation time.</p>
+  <p>The main goal is to achieve the greater market share in the end of the
+      simulation.</p>
+  <hr>
+  <h2>Partners</h2>
+  <h3 style="text-align: left">
+      Stimulating Learning in Engineering Students by Collaborative Entrepreneurship
+      Training</h3>
+  <p>This tool is part of project StimuLearning project, that aims to promote improved
+      learning and entrepreneurial spirit among engineering students by resorting to
+      the simulation of real world business cases in a collaborative mobile digital
+      environment. </p>
+  <p>This project implements project-based learning combined with an interactive
+      entrepreneurial atmosphere where several industry professionals participate,
+      helping students to create real world business cases, with different teams
+      playing the roles of competing companies in a marketplace.</p>
+  <p>Stimulearning is a project from Bandwidth Systems Group (GSBL) of Aveiro
+      University. <a href="http://gsbl.det.ua.pt/gsbl_en/hp-stimulearning.aspx">More info...</a> </p>
 </div>
