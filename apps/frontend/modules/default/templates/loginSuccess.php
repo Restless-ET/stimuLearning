712b914c789f5800f4be322ef3fbbdf6b011308c
Artur Daniel Melo <adsmelo@ua.pt>
2013-11-13 10:25:32 +0000

    Improve looks of login page.

--- apps/frontend/modules/default/templates/loginSuccess.php
+++ apps/frontend/modules/default/templates/loginSuccess.php
@@ -1,10 +1,7 @@
-<?php if($sf_user->getFlash('created')): ?>
-<font color="green">Conta criada com sucesso! Aguarde a validação da sua conta para poder aceder ao sistema.</font><br />
-<?php endif; ?>
-<fieldset>
-  <legend>Access Credentials</legend>
-  <form action="<?php echo url_for('@login') ?>" method="POST">
-    <table style="border-spacing: 0px;">
+<fieldset style="border: solid 1px black;">
+  <legend style="margin-left: 10px;">&nbsp;Access Credentials&nbsp;</legend>
+  <form action="<?php echo url_for('@login') ?>" method="POST" style="margin-left: 10px;">
+    <table style="margin-top: 0.5em;">
         <?php echo $form ?>
       <tr>
         <td colspan="2">
