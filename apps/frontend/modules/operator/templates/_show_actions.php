3cc546ee90809066b1e1b21ff3b6faa5adebec78
Artur Daniel Melo <adsmelo@ua.pt>
2013-11-15 15:46:10 +0000

    See #38 Fix checking for actions on operator list page

--- apps/frontend/modules/operator/templates/_show_actions.php
+++ apps/frontend/modules/operator/templates/_show_actions.php
@@ -1,7 +1,7 @@
 <ul class="sf_admin_actions_form">
   <?php echo $helper->linkToList(array(  'params' => 'class= fg-button ui-state-default ui-corner-all fg-button-icon-left ',  'class_suffix' => 'list',  'label' => 'Back to list',  'ui-icon' => '',)) ?>
 
-<?php if ($operator->Scenario->started): ?>
+<?php if (!$operator->Scenario->started): ?>
   <?php if ($sf_user->hasCredential(array(  0 =>   array(    0 => 'admin',    1 => 'responsible',  ),))): ?>
     <?php echo $helper->linkToEdit($operator, array(  'credentials' =>   array(    0 =>     array(      0 => 'admin',      1 => 'responsible',    ),  ),  'params' => 'class= fg-button ui-state-default ui-corner-all fg-button-icon-left ',  'class_suffix' => 'edit',  'label' => 'Edit',  'ui-icon' => '',)) ?>
   <?php endif; ?>
